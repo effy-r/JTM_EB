f4e6c70e2917f4924a61703d3c99975a5c326d72
effy-r <dustyboness@gmail.com>
2020-01-27 18:20:45 +0200

    ArrayListMethods done

--- src/main/java/jtm/extra02/ArrayListMethods.java
+++ src/main/java/jtm/extra02/ArrayListMethods.java
@@ -1,42 +1,41 @@
 package jtm.extra02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArrayListMethods {
 
 	List<Integer> myList = new ArrayList<Integer>();
-	List<Integer> newList = new ArrayList<Integer>();
 
 	public List<Integer> checkArray(int comparator, int... numbers) {
 
-		for (int i = 0; i < myList.size(); i++) {
+		// int j = 0;
 
-			if (myList.get(i) < comparator) {
+		for (int i = 0; i < numbers.length; i++) {
 
-				for (int j = 0; j < myList.size(); j++) {
+			if (numbers[i] < comparator) {
 
-					newList.set(j, myList.get(i));
-
-				}
+				myList.add(numbers[i]);
 
 			}
-			// TODO #1:Implement method that compares values of passed array with
-			// passed comparator.
-			// Return list with values that are smaller than comparator.
-			// Hint: Investigate how varargs are used.
-
 		}
-		return newList;
+		return myList;
+
+		// TODO #1:Implement method that compares values of passed array with
+		// passed comparator.
+		// Return list with values that are smaller than comparator.
+		// Hint: Investigate how varargs are used.
+
 	}
 
 	public int sumResult() {
 		int sum = 0;
 		// TODO #2: Count element sum of the list
 
-		for (int i = 0; i < newList.size(); i++) {
-			
-			sum = sum + newList.get(i);
+		for (int i = 0; i < myList.size(); i++) {
+
+			sum = sum + myList.get(i);
 
 		}
 		return sum;
