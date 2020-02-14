45a3896800c1048cffd00b06da0f3ddfd46d7368
effy-r <dustyboness@gmail.com>
2020-02-14 17:29:29 +0200

    Changes in CopyFile, Person and PersonRepo

--- src/main/java/jtm/activity10/CopyFile.java
+++ src/main/java/jtm/activity10/CopyFile.java
@@ -2,6 +2,7 @@ package jtm.activity10;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,29 +13,36 @@ public class CopyFile {
 	public static void main(String[] args) {
 	
 		
-	
+		BufferedReader reader;
+		PrintWriter writer;
 	
 	File rightFile = new File ("C:\\Users\\evija\\Desktop\\newdocument.txt");
 	//String newFilePath = filePath + "_backup";
 	File copyFile = new File ("C:\\Users\\evija\\Desktop\\newdocument.txt_backup");
 	
 	try {
+		if (copyFile.createNewFile()) {
+			
+		reader = new BufferedReader (new FileReader(rightFile));
 		
-		BufferedReader in = new BufferedReader (new FileReader(rightFile));
+		writer = new PrintWriter(new FileWriter(copyFile));
 		
-		//List<String> linesInList = Files.readAllBytes(Paths.get(rightFile));
-		//Files.newBufferedWriter(Path.get(path), lines);
-		PrintWriter out = new PrintWriter(new FileWriter(copyFile));
 		String s;
-		while ((s = in.readLine())!= null) {
-			out.println(s);
-		}
-		in.close();
-		out.close();
 		
+		while ((s = reader.readLine())!= null) {
+			
+			writer.println(s);
+			
+		}
+		reader.close();
+		writer.close();
+		}
 	} catch (IOException e) {
 		e.printStackTrace();
 	}
+	//catch (FileNotFoundException f) {
+		//f.printStackTrace();
+	//}
 	//C:\Users\evija\Desktop
 }
 }
--- src/main/java/jtm/activity10/PersonRepo.java
+++ src/main/java/jtm/activity10/PersonRepo.java
@@ -1,5 +1,10 @@
 package jtm.activity10;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Paths;
+
 /*
 TODO Necessary imports commented
  */
@@ -12,53 +17,66 @@ import java.util.*;
 
 public class PersonRepo {
 
-    private List<Person> personList;
+	private List<Person> personList;
 
-    public PersonRepo() {
-        init();
-    }
+	public PersonRepo() {
+		init();
+	}
 
-    public void init() {
+	public void init() {
 
-        try {
-            // 1. Read file data.json from resources folder into String
-            // 2. Use ObjectMapper to convert String to List of Persons and
-            // store in personList field
-            //Hint: Correct way how to use ObjectMapper to convert String to List below
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		try {
 
-    public Person oldestPerson() {
-        //Find oldest person in personList field and return it
-        return null;
-    }
+			File fileData = new File("\\data.json");
+			BufferedReader reader = new BufferedReader(new FileReader(fileData));
+			List<String> linesInList = Files.readAllBytes(Paths.get(fileData));
+			List<String> presonList = new List<String>;
 
-    public Person youngestPerson() {
-        //Find youngest person in personList field and return it
-        return null;
-    }
+			// Files.newBufferedWriter(Path.get(path), lines);
+			
+			// 1. Read file data.json from resources folder into String
+			// 2. Use ObjectMapper to convert String to List of Persons and
+			// store in personList field
+			// Hint: Correct way how to use ObjectMapper to convert String to List below
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public String largestPopulation() {
-        //Find country with largest population and return it's name
-        return null;
-    }
+	public Person oldestPerson() {
+		
+		Person OldestPerson = new Person();
+		for (int i=0; i<size.personList; i++) {
+			if (get.personList(i)>OldestPerson) {
+				OldestPerson = get.personList(i);
+			}
+		}
+		// Find oldest person in personList field and return it
+		return OldestPerson;
+	}
 
+	public Person youngestPerson() {
+		
+		Person YoungestPerson = new Person();
+		for (int i=0; i<size.personList; i++) {
+			if (get.personList(i)<YoungestPerson) {
+				YoungestPerson = get.personList(i);
+			}
+		}
+		// Find youngest person in personList field and return it
+		return YoungestPerson;
+	}
+
+	public String largestPopulation() {
+		// Find country with largest population and return it's name
+		return null;
+	}
 
 }
 
-
-
-
-
-
-
-
 /*
-
-Hint:
-            ObjectMapper mapper = new ObjectMapper();
-            Person[] personArray = mapper.readValue(data, Person[].class);
-            List<Person> personList = Arrays.asList(personArray);
+ * 
+ * Hint: ObjectMapper mapper = new ObjectMapper(); Person[] personArray =
+ * mapper.readValue(data, Person[].class); List<Person> personList =
+ * Arrays.asList(personArray);
  */
