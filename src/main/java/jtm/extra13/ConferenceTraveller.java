31e976e942b365d4a0c8d96f0acaaa2f8e1d4daa
effy-r <dustyboness@gmail.com>
2020-02-21 20:24:12 +0200

    Extra13

--- src/main/java/jtm/extra13/ConferenceTraveller.java
+++ src/main/java/jtm/extra13/ConferenceTraveller.java
@@ -4,38 +4,76 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ConferenceTraveller {
-    /*
-        TODO
+	/*
+	 * TODO
+	 * 
+	 * Lucy loves to travel. Luckily she is a renowned computer scientist and gets
+	 * to travel to international conferences using her department's budget.
+	 * 
+	 * Each year, Society for Exciting Computer Science Research (SECSR) organizes
+	 * several conferences around the world. Lucy always picks one conference from
+	 * that list that is hosted in a city she hasn't been to before, and if that
+	 * leaves her with more than one option, she picks the conference that she
+	 * thinks would be most relevant for her field of research.
+	 * 
+	 * citiesVisited, a list of cities that Lucy has visited before, given as an
+	 * array of strings. citiesOffered, a list of cities that will host conferences
+	 * this year, given as an array of strings, ordered in terms of the relevance of
+	 * the conferences for Lucy's research (from the most to the least relevant).
+	 * 
+	 * 
+	 * Example: citiesVisited =
+	 * ["Mexico City","Johannesburg","Stockholm","Osaka','Saint
+	 * Petersburg','London']; citiesOffered = ["Stockholm","Paris","Melbourne"];
+	 * Correct answer: 'Paris' (City that has not been visited before, and has the
+	 * highest priority)
+	 */
 
-        Lucy loves to travel. Luckily she is a renowned computer scientist and gets to travel to international
-        conferences using her department's budget.
+	public static String conferencePicker(String[] citiesVisited, String[] citiesOffered) {
 
-        Each year, Society for Exciting Computer Science Research (SECSR) organizes several
-        conferences around the world. Lucy always picks one conference from that list that
-        is hosted in a city she hasn't been to before, and if that leaves her with more than one option,
-        she picks the conference that she thinks would be most relevant for her field of research.
+		String conferenceCity = null;
+		int index = 0;
 
-        citiesVisited, a list of cities that Lucy has visited before, given as an array of strings.
-        citiesOffered, a list of cities that will host conferences this year, given as an array of strings,
-            ordered in terms of the relevance of the conferences for Lucy's research (from the most to the least relevant).
+		if (citiesVisited.length > 0) {
 
+			for (int j = 0; j < citiesOffered.length; j++) {
 
-        Example:
-        citiesVisited = ["Mexico City","Johannesburg","Stockholm","Osaka','Saint Petersburg','London'];
-        citiesOffered = ["Stockholm","Paris","Melbourne"];
-        Correct answer:  'Paris' (City that has not been visited before, and has the highest priority)
-     */
+				for (int i = 0; i < citiesVisited.length; i++) {
 
-    public static String conferencePicker(String[] citiesVisited, String[] citiesOffered) {
-       return null;
-    }
+					if (citiesOffered[j] == citiesVisited[i]) {
+						index = 0;
+						System.out.println("Have been in the city: " + citiesOffered[j]);
+						break;
+					} else {
+						index = j;
+					}
+				}
 
-    public static void main(String[] args) {
-        String city = conferencePicker(
-                new String[]{"Mexico City", "Johannesburg", "Stockholm", "Osaka", "Saint Petersburg", "London"},
-                new String[]{"Stockholm", "Paris", "Melbourne"}
-        );
-        System.out.println(city);
-    }
+				if (index > 0) {
+
+					System.out.println("Have not been in the city: " + citiesOffered[j]);
+					conferenceCity = citiesOffered[j];
+					break;
+				}
+
+				if (index == 0) {
+					conferenceCity = "No worthwhile conferences this year!";
+				}
+			}
+		} else if (citiesVisited.length == 0) {
+			conferenceCity = citiesOffered[0];
+
+		}
+		;
+
+		return conferenceCity;
+	}
+
+	public static void main(String[] args) {
+		String city = conferencePicker(
+				new String[] { "Mexico City", "Johannesburg", "Stockholm", "Osaka", "Saint Petersburg", "London" },
+				new String[] { "Stockholm", "Paris", "Melbourne" });
+		System.out.println(city);
+	}
 
 }
